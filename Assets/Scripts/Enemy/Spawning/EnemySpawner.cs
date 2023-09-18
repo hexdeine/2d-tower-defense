8c6038dfccfde69ca983c1c659b1de0f4ded5e7c
Sabin Cacho <sabinmiguelcacho01@gmail.com>
2023-09-18 20:56:05 +0800

    enemy spawner: started work on enemy spawner - added waypoint initialization - added game manager script file

--- Assets/Scripts/Enemy/Spawning/EnemySpawner.cs
+++ Assets/Scripts/Enemy/Spawning/EnemySpawner.cs
@@ -1,8 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    int enemyCount = 2;
+    public int enemyCount = 2;
+    public int enemySpawnRate = 1;
+    public Transform waypoints;
+    public GameObject enemyPrefab;
+
+    private List<Transform> Waypoints;
+    private int enemiesSpawned = 0;
+    private GameObject enemyParent;
+    private bool canSpawnEnemy = true;
+
+    private void Awake() {
+        InitializeWaypoints();
+    }
+
+    private void Update() {
+        SpawnEnemy();
+    }
+
+    private void InitializeWaypoints() {
+        Waypoints = new List<Transform>();
+
+        foreach (Transform child in waypoints) {
+            Waypoints.Add(child);
+        }
+    }
+
+    private void SpawnEnemy() {
+        if (!canSpawnEnemy) return;
+
+
+    }
 }
